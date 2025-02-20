ffa32084595e69fdaa9176cd92bafe602b5712a9
garima-l5 <garima.sachdeva@layerfive.com>
2025-02-20 11:09:13 +0530

    authentication

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,12 +88,40 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ENVIRONMENT = os.getenv('DJANGO_ENV', 'development')  # Default to 'development'
+
+if ENVIRONMENT == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'your_prod_db'),
+            'USER': os.getenv('POSTGRES_USER', 'your_prod_user'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'your_prod_password'),
+            'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'treeo',
+#         'USER': 'treeo',
+#         'PASSWORD': 'Bhupa@898',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 
 # Password validation
--- users/views.py
+++ users/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+import traceback
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import User, UserRole
 
-# Create your views here.
+
+def register(request):
+    role_id = request.GET.get('role')
+    role = UserRole.objects.get(user_type=role_id)
+    if request.method == 'POST':
+        try:
+            full_name = request.POST.get('full_name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+
+            if password != confirm_password:
+                messages.error(request, "Passwords do not match!")
+                return redirect('signup')
+
+            try:
+                role = UserRole.objects.get(user_type=role_id)
+            except UserRole.DoesNotExist:
+                messages.error(request, "Invalid user role selected.")
+                return redirect('signup')
+
+            User.objects.create_user(
+                email=email,
+                first_name=full_name,
+                phone=phone,
+                password=password,
+                role=role
+            )
+
+            messages.success(request, "Account created! Please log in.")
+            return redirect('login')
+        except:
+            traceback.print_exc()
+
+    return render(request, 'signup.html')
+
+
+
+
+def login(request):
+    # if request.method == 'POST':
+    #     email = request.POST.get('email')
+    #     password = request.POST.get('password')
+    #     user = authenticate(request, email=email, password=password)
+
+    #     if user:
+    #         login(request, user)
+    #         messages.success(request, "Logged in successfully!")
+    #         return redirect('db-dashboard')
+    #     else:
+    #         messages.error(request, "Invalid email or password.")
+
+    return render(request, 'login.html')
+
+
+
+def logout(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect('login')
+
+
